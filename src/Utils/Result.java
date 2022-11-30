777d1add26beecc5a8a820bdd1b6bc723a7c44da
Nathan <tzy15368@outlook.com>
2022-11-30 18:09:35 -0500

    add Result wrapper for public methods

---
+++ src/Utils/Result.java
@@ -0,0 +1,45 @@
+package Utils;
+
+public class Result<T> {
+    private boolean success;
+    private String msg;
+    private T data;
+
+    public Result() {
+    }
+
+    public Result(boolean success, String msg, T data) {
+        this.success = success;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return (this.success ? "ok" : "fail") + ": " + this.msg;
+    }
+}
