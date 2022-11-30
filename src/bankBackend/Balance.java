978e98ad977e5ff79d5274b5ec0b0f48c1894529
Nathan <tzy15368@outlook.com>
2022-11-30 18:03:07 -0500

    add placeholder for balance and tx

--- src/bankBackend/Balance.java
+++ src/bankBackend/Balance.java
@@ -1,30 +1,39 @@
 package bankBackend;
 
-import java.util.HashMap;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 
+enum CurrencyType {
+    USD,
+    EUR,
+    GBP,
+}
+
+@DatabaseTable(tableName = "Balances")
 public class Balance {
+    @DatabaseField
+    private int userId;
+    @DatabaseField
     private double value;
-    private String kind;
+    @DatabaseField
+    private CurrencyType type;
 
-    public Balance(double value, String kind){
-        this.value=value;
-        this.kind=kind;
+    public Balance(double value, CurrencyType kind) {
+        this.value = value;
+        this.type = kind;
     }
 
-    public void addBalance(double value, String kind){
+    public void addBalance(double value, String kind) {
         //if exist, add money, if not exist, create new balance
     }
 
-    public void minusBalance(double value, String kind){
+    public void minusBalance(double value, String kind) {
 
     }
 
-    public double findBalance(String kind){
+    public double findBalance(String kind) {
         return value;
     }
-
-
-
 
 
 }
