adc78e898f2854033f0c1464580b58c9ab915b9c
Supreelaaa <shuxing1997@gmail.com>
2022-12-06 01:25:14 -0500

    update Report and Timer

--- src/Utils/Result.java
+++ src/Utils/Result.java
@@ -25,14 +25,28 @@ public class Result<T> {
         this.data = data;
     }
 
-    // data MUST NOT be null
-    // USE WITH CAUTION, UNWRAP MAY FAIL CATASTROPHICALLY AND CALL SYSTEM.EXIT(-1)
-    // SEE IMPL OF LOGGER.FATAL
-    public T unwrap() {
-        if (this.data == null) {
-            Logger.fatal("Result unwrap failed, data is null");
-        }
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
         return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 
     @Override
--- src/bankBackend/Report.java
+++ src/bankBackend/Report.java
@@ -1,5 +1,32 @@
 package bankBackend;
 
-public class Report {
+import Utils.Logger;
+import Utils.Result;
 
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+public class Report {
+    private int date;
+    private List<Transaction> transactionList;
+
+    public Report(int date){
+        this.date=date;
+        this.transactionList=setTransaction(date).getData();
+    }
+
+    private Result<List<Transaction>> setTransaction(int date){
+        try {
+            List<Transaction> transactionList=Transaction.dao.queryBuilder().where().eq("date", date).query();
+            return new Result<>(true, "getTransaction: success", transactionList);
+        }catch (SQLException e) {
+            Logger.error("SQL Exception in getTransaction:"+e+e.getMessage());
+            return new Result<>(false, "getTransaction: " + e.getMessage(), null);
+        }
+    }
+
+    public List<Transaction> getTransactionList() {
+        return transactionList;
+    }
 }
